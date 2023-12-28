324938ec05698a28140264f1a94817006211d8fc
nilaelang <117297100+nilaelang@users.noreply.github.com>
2023-12-28 12:58:13 +0530

    Added functions for deleting node

--- Linked List.c
+++ Linked List.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node {
+struct node 
+{
     int info;
     struct node *link;
 };
+//struct node *createList();
+//struct node *add_beg(struct node *start, int data);
+//struct node *add_end(struct node *start, int data);
+//struct node *beforeAnode(struct node *start,int data, int item);
+//struct node *afterAnode(struct node *start,int data, int item);
+//struct node *atAposition(struct node *start,int data, int position);
+//void display(struct node *start);
+//struct node *del_beg(struct node *start, int data);
+//struct node *del_end(struct node *start, int data);
+//struct node *DelbeforeAnode(struct node *start,int data, int item);
+//struct node *DelafterAnode(struct node *start,int data, int item);
+//struct node *DelatAposition(struct node *start,int data, int position);
 
-struct node *createList();
-struct node *add_beg(struct node *start, int data);
-struct node *add_end(struct node *start, int data);
-struct node *beforeAnode(struct node *start,int data, int item);
-struct node *afterAnode(struct node *start,int data, int item);
-struct node *atAposition(struct node *start,int data, int position);
-void display(struct node *start);
-
-struct node *createList() {
+//function to create a linked list 
+struct node *createList() 
+{
     struct node *start = NULL;
     struct node *temp, *newNode;
     int size, data, i;
-
     printf("Enter the number of nodes: ");
     scanf("%d", &size);
-
     if (size <= 0) {
         printf("Invalid size.\n");
         return NULL;
     }
-
     printf("Enter the elements: \n");
-
     for (i = 0; i < size; i++) {
         newNode = (struct node *)malloc(sizeof(struct node));
-        if (newNode == NULL) {
+        if (newNode == NULL) 
+		{
             printf("Memory allocation failed.\n");
             return NULL;
         }
-
         printf("Enter element %d: ", i + 1);
         scanf("%d", &data);
-
         newNode->info = data;
         newNode->link = NULL;
-
         if (start == NULL) {
             start = newNode;
         } else {
@@ -54,32 +54,30 @@ struct node *createList() {
     }
     return start;
 }
-
-struct node *add_beg(struct node *start, int data) {
+//function to add a node at the begining of the list
+struct node *add_beg(struct node *start, int data) 
+{
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return start;
     }
-
     newNode->info = data;
     newNode->link = start;
     start = newNode;
-
     printf("Node inserted at the beginning successfully.\n");
     return start;
 }
-
-struct node *add_end(struct node *start, int data) {
+//function to add a node at the end of the list
+struct node *add_end(struct node *start, int data) 
+{
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return start;
     }
-
     newNode->info = data;
     newNode->link = NULL;
-
     if (start == NULL) {
         start = newNode; 
     } else {
@@ -89,128 +87,114 @@ struct node *add_end(struct node *start, int data) {
         }
         temp->link = newNode; 
     }
-
     printf("Node inserted at the end successfully.\n");
     return start; 
 }
-
-struct node *atAposition(struct node *start, int data, int position) {
+//function to add at a given position 
+struct node *atAposition(struct node *start, int data, int position) 
+{
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return start;
     }
-
     newNode->info = data;
     newNode->link = NULL;
-
     if (position == 1) {
         newNode->link = start;
         start = newNode;
-    } else {
+    } 
+	else 
+	{
         struct node *temp = start;
         int count = 1;
-
         while (count < position - 1 && temp != NULL) {
             temp = temp->link;
             count++;
         }
-
         if (temp == NULL) {
             printf("Position is out of range.\n");
             free(newNode);
             return start;
         }
-
         newNode->link = temp->link;
         temp->link = newNode;
     }
-
     printf("Node inserted at position %d successfully.\n", position);
     return start;
 }
-
-struct node *beforeAnode(struct node *start, int data, int key) {
+//function to add a node before a given node
+struct node *beforeAnode(struct node *start, int data, int key) 
+{
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return start;
     }
-
     newNode->info = data;
     newNode->link = NULL;
-
     if (start == NULL) {
         printf("List is empty.\n");
         free(newNode);
         return start;
     }
-
     if (start->info == key) {
         newNode->link = start;
         start = newNode;
         printf("Node inserted before the first node successfully.\n");
         return start;
     }
-
     struct node *temp = start;
     while (temp->link != NULL && temp->link->info != key) {
         temp = temp->link;
     }
-
     if (temp->link == NULL) {
         printf("Node with key %d not found.\n", key);
         free(newNode);
         return start;
     }
-
     newNode->link = temp->link;
     temp->link = newNode;
-
     printf("Node inserted before %d successfully.\n", key);
     return start;
 }
-
-struct node *afterAnode(struct node *start, int data, int key) {
+//function to add node after a given node
+struct node *afterAnode(struct node *start, int data, int key) 
+{
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return start;
     }
-
     newNode->info = data;
     newNode->link = NULL;
-
     if (start == NULL) {
         printf("List is empty.\n");
         free(newNode);
         return start;
     }
-
     struct node *temp = start;
-    while (temp != NULL && temp->info != key) {
+    while (temp != NULL && temp->info != key) 
+	{
         temp = temp->link;
     }
-
     if (temp == NULL) {
         printf("Node with key %d not found.\n", key);
         free(newNode);
         return start;
     }
-
     newNode->link = temp->link;
     temp->link = newNode;
-
     printf("Node inserted after %d successfully.\n", key);
     return start;
 }
-
-void display(struct node *start) {
+//function to display the created linked list 
+void display(struct node *start) 
+{
     if (start == NULL) {
         printf("List is empty.\n");
         return;
     }
-
     struct node *temp = start;
     printf("Elements in the linked list are: ");
     while (temp != NULL) {
@@ -219,19 +203,130 @@ void display(struct node *start) {
     }
     printf("\n");
 }
+// Function to delete the node at the beginning of the list
+struct node *del_beg(struct node *start, int data) 
+{
+    if (start == NULL) {
+        printf("List is empty, deletion not possible.\n");
+        return start;
+    }
+    struct node *temp = start;
+    start = start->link;
+    free(temp);
+    printf("Node deleted from the beginning successfully.\n");
+    return start;
+}
+ // Function to delete the node at the end of the list
+struct node *del_end(struct node *start, int data) 
+{
+    if (start == NULL) {
+        printf("List is empty, deletion not possible.\n");
+        return start;
+    }
+    struct node *ptr = start;
+    struct node *prev = NULL;
+    while (ptr->link != NULL) {
+        prev = ptr;
+        ptr = ptr->link;
+    }
+    if (prev == NULL) {
+        free(ptr);
+        start = NULL;
+    } else {
+        prev->link = NULL;
+        free(ptr);
+    }
+    printf("Node deleted from the end successfully.\n");
+    return start;
+}
+// Function to delete the node before a specified node with 'item'
+struct node *DelbeforeAnode(struct node *start, int data, int item) 
+{
+    if (start == NULL || start->link == NULL) {
+        printf("List is empty or contains only one node, deletion not possible.\n");
+        return start;
+    }
+    struct node *ptr = start;
+    struct node *prev = NULL;
+    while (ptr->link != NULL && ptr->link->info != item) {
+        prev = ptr;
+        ptr = ptr->link;
+    }
+    if (ptr->link == NULL || ptr == start) {
+        printf("Node with given item not found or it's the first node, deletion not possible.\n");
+        return start;
+    }
+    struct node *temp = prev->link;
+    prev->link = ptr->link;
+    free(temp);
+    printf("Node before %d deleted successfully.\n", item);
+    return start;
+}
+// Function to delete the node after a specified node with 'item'
+struct node *DelafterAnode(struct node *start, int data, int item) 
+{
+    if (start == NULL) {
+        printf("List is empty, deletion not possible.\n");
+        return start;
+    }
+    struct node *ptr = start;
+    while (ptr != NULL && ptr->info != item) {
+        ptr = ptr->link;
+    }
+    if (ptr == NULL || ptr->link == NULL) {
+        printf("Node with given item not found or it's the last node, deletion not possible.\n");
+        return start;
+    }
+    struct node *temp = ptr->link;
+    ptr->link = temp->link;
+    free(temp);
+    printf("Node after %d deleted successfully.\n", item);
+    return start;
+}
+// Function to delete a node at a specified position in the list
+struct node *DelatAposition(struct node *start, int data, int position) 
+{
+    if (start == NULL) {
+        printf("List is empty, deletion not possible.\n");
+        return start;
+    }
+    if (position == 1) {
+        struct node *temp = start;
+        start = start->link;
+        free(temp);
+        printf("Node at position %d deleted successfully.\n", position);
+        return start;
+    }
+    struct node *ptr = start;
+    struct node *prev = NULL;
+    int count = 1;
+    while (ptr != NULL && count != position) {
+        prev = ptr;
+        ptr = ptr->link;
+        count++;
+    }
+    if (ptr == NULL) {
+        printf("Position out of range, deletion not possible.\n");
+        return start;
+    }
+    prev->link = ptr->link;
+    free(ptr);
+    printf("Node at position %d deleted successfully.\n", position);
+    return start;
+}
 
-int main() {
+int main() 
+{
     int choice, choice1, choice2, data,item,position;
     struct node *list = NULL;
     int exitFlag = 0;
 
-    while (!exitFlag) 
+    while(!exitFlag) 
 	{
         printf("MAIN MENU\n");
         printf("1. Create a linked list \n2. Insert \n3. Display the Linked List\n4. Delete \n5. Exit\n");
-        printf("Enter your choice\n");
-        scanf("%d", &choice);
-
+        printf("Enter your choice :");
+        scanf("%d",&choice);
         switch (choice) {
             case 1:
                 list = createList();
@@ -242,7 +337,8 @@ int main() {
                 printf("1. At the beginning\n2. At the end\n3. In the middle of the list\n");
                 printf("Enter your choice\n");
                 scanf("%d", &choice1);
-                switch (choice1) {
+                switch (choice1) 
+				{
                     case 1:
                         printf("Enter data to be inserted at the beginning : ");
                         scanf("%d", &data);
@@ -288,8 +384,7 @@ int main() {
 								break;
 							default :
 								printf("Invalid option entered \n");
-							}
-						
+							}	
                     default:
                         printf("Invalid option entered \n");
                 }
@@ -299,8 +394,52 @@ int main() {
                 break;
             case 4:
                 printf("DELETE A NODE\n");
-                
-                printf("The function for deleting a node is not ADDED yet \n ");
+                printf("1. Delete at the beginning\n2. Delete at the end\n3. Delete in the middle of the list\n");
+                printf("Enter your choice\n");
+                scanf("%d", &choice1);
+                switch (choice1) 
+				{
+                    case 1:
+                    	printf("Enter data to be deleted at the beginning : ");
+                        scanf("%d", &data);
+                        list = del_beg(list, data);
+                        display(list);
+                        break;
+                    case 2:
+                    	printf("Enter data to be deleted at the end : ");
+                        scanf("%d", &data);
+                        list = del_end(list, data);
+                        display(list);
+                        break;
+                    case 3:
+                    	printf("Delete in the middle of the list \n");
+                    	printf("1. Delete before a node\n2. Delete after a node\n3. Delete at a position\n");
+                    	printf("Enter your choice : ");
+                    	scanf("%d",&choice);
+                    	switch(choice)
+                    	{
+	                    	case 1:
+		                        printf("Enter the item before which the node has to be deleted: ");
+		                        scanf("%d", &item);
+		                        list = DelbeforeAnode(list, data, item);
+		                        break;
+	                    	case 2:
+		                        printf("Enter the item after which the node has to be deleted: ");
+		                        scanf("%d", &item);
+		                        list = DelafterAnode(list, data, item);
+		                        break;
+	                    	case 3:
+		                        printf("Enter the position of the node to be deleted: ");
+		                        scanf("%d", &position);
+		                        list = DelatAposition(list, data, position);
+		                        break;
+		                    default :
+		                    	printf("Invalid choice \n");
+		            	}
+                    default:
+                        printf("Invalid choice for deletion.\n");
+                        break;
+                }
                 break;
             case 5:
                 exitFlag = 1; 
@@ -310,6 +449,6 @@ int main() {
         }
     }
 
-    printf("Exiting the program. Thank you!\n");
+    printf("Exiting the program...\nThank you!\n");
     return 0;
 }
